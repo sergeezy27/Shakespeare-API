db2f9e01d16399f4203ec2058d3463d5b702b57b
Sergei Vorobev <svorobyovs357@gmail.com>
2024-11-25 16:36:08 -0600

    Improved login page skeleton

--- core/SSI/bottom.php
+++ core/SSI/bottom.php
@@ -1,6 +1,6 @@
     </div>
     <div id="footer" class="footer">
-
+        <span id="copyright">Copyright &copy; 2024 Sergei Vorobev. All rights reserved.</span>
     </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,7 +4,9 @@ $security = false;
 require 'core/SSI/top.php';
 ?>
 
-<img src="graphics/flowers-from-shakespeare-s-garden-clipart.svg" alt="Woman in front of a Shakespeare statue" height="600"/>
+<img src="graphics/flowers-from-shakespeare-s-garden-clipart.svg" alt="Woman in front of a Shakespeare statue" class="shakespeare-svg"/>
+
+<div class="content">Place holder</div>
 
 <?
 require 'core/SSI/bottom.php';
